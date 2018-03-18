1c086d603c8e2b5e232346b737cb9b8ef5b7edd8
Yumi Kondo <yumik091186@gmail.com>
2018-03-18 16:54:25 -0700

    initial commit

---
+++ backend.py
@@ -0,0 +1,193 @@
+from json import loads
+from urllib2 import Request, urlopen
+import sys
+import pandas as pd
+sys.path.append("app/")
+from personal import MY_API_KEY
+
+
+GoogleMap_API="https://maps.googleapis.com/maps/api/"
+
+
+def get_path_and_altitutde(origin,destination):
+    '''
+
+    :param
+    origin: Point object (lat,lng)
+    destination: Point object (lat,lng)
+    :return:
+    locations: list containing (lat,lng) to connect the origin and destination
+    '''
+    km, dur = 0, 0
+    info = [{"lat":origin.latlng[0],
+             "lng":origin.latlng[1],
+             "placename":origin.placename,
+             "altitude":origin.altitude,
+             "distance":km,
+              "duration":dur}]
+    req = request_path(origin.latlng,destination.latlng)
+    try:
+        places = request_load(req)
+        for r in places["routes"][0]["legs"][0]["steps"]:
+            km += r["distance"]["value"]
+            dur += r["duration"]["value"]/60.0
+            startdict = r["start_location"]
+            startdict["placename"] = ""
+            startdict["distance"] = km
+            startdict["duration"] = dur
+            ## request altitude
+            req = request_alt((startdict["lat"],
+                               startdict["lng"]))
+            places = request_load(req)
+            startdict["altitude"] = places['results'][0]['elevation'];
+            info.append(startdict)
+    except Exception as e:
+        print(e)
+        km = 1
+        dur = 1
+        pass
+    info.append({"lat": destination.latlng[0],
+                 "lng": destination.latlng[1],
+                 "placename": destination.placename,
+                  "altitude":destination.altitude,
+                  "distance":km,
+                  "duration":dur})
+    info = pd.DataFrame(info)
+    return(info)
+
+def request_path(origin,dest):
+    requeststr = GoogleMap_API + 'directions/json?origin={},{}&destination={},{}&key={}&mode=walking'.format(
+                  origin[0],origin[1],
+                  dest[0],dest[1],MY_API_KEY)
+    return(requeststr)
+
+def request_alt(loc):
+    requeststr = GoogleMap_API + 'elevation/json?locations={},{}&key={}'.format(
+    loc[0], loc[1], MY_API_KEY)
+    return(requeststr)
+
+
+def request_load(requeststr):
+    request  = Request(requeststr)
+    response = urlopen(request).read()
+    places   = loads(response)
+    return(places)
+
+class Point(object):
+    def __init__(self,place,detail,ID):
+        self.placename = place
+        self.latlng    = self.find_latlng_textsearch([place,"Nepal"])
+        self.altitude = self.find_altitude(self.latlng)
+        self.schedule  = detail
+        self.day = ID
+        self.path2next = None
+
+    def add_path(self,path):
+        self.path2next = path
+
+
+    def find_latlng_geocoding(self,keywords):
+        '''
+        using google map API find the lat long given an address created with keywwords
+        e.g., keywords = ["Nepal","Lukila"]
+        :return:
+        '''
+        address = keywords[0].replace(" ","+")
+        for keyword in keywords[1:]:
+            address += "+"+keyword.replace(" ","+")
+        requeststr = GoogleMap_API + "geocode/json?address={}&key={}".format(
+            address,MY_API_KEY)
+        try:
+            places = request_load(requeststr)
+            loc = places["results"][0]["geometry"]["location"]
+        except:
+            loc ={"lat":None,"lng":None}
+        return (loc["lat"],loc["lng"])
+
+    def find_latlng_textsearch(self,keywords):
+        '''
+
+        :param keywords:
+        :return:
+        '''
+        address = keywords[0].replace(" ","+")
+        for keyword in keywords[1:]:
+            address += "+" + keyword.replace(" ","+")
+        requeststr = GoogleMap_API + "place/textsearch/json?query={}&key={}".format(
+            address,MY_API_KEY)
+        try:
+            places = request_load(requeststr)
+            loc = places["results"][0]["geometry"]["location"]
+        except Exception as e:
+            print(e)
+            print(address)
+            print(requeststr)
+            loc = {"lat": None, "lng": None}
+        return (loc["lat"], loc["lng"])
+
+    def find_altitude(self,loc):
+        requeststr = GoogleMap_API + 'elevation/json?locations={},{}&key={}'.format(
+                      loc[0], loc[1], MY_API_KEY)
+        try:
+            places = request_load(requeststr)
+            elevation  = places['results'][0]['elevation'];
+        except Exception as e:
+            elevation = -1;
+        return elevation
+
+class Trip(object):
+    def __init__(self):
+        self.points = []
+    def add_point(self,point):
+        self.points.append(point)
+
+if __name__ == '__main__':
+
+
+    file = pd.read_csv("app/static/data/mytrip.csv")
+    ## define mytrip object
+    mytrip = Trip()
+    for irow in range(file.shape[0]):
+        row = file.iloc[irow,:]
+        mypoint = Point(row["placename"],row["schedule"],row["Day"])
+        mytrip.add_point(mypoint)
+
+    for point in mytrip.points:
+        print(point.placename, point.latlng)
+
+
+
+
+    for start, end in zip(mytrip.points[:-1],mytrip.points[1:]):
+        info = get_path_and_altitutde(start,end)
+        start.add_path(info)
+
+
+    for i in range(len(mytrip.points)):
+        print(mytrip.points[i].path2next)
+
+
+    ## create a csv containing
+    ## ID placename, description, latitude, lontitude, altitude
+    text = []
+
+    for pid,point in enumerate(mytrip.points):
+        pid_string = "{:05.0f}".format(pid)
+        text.append([pid_string,
+                     point.day,
+                     point.placename,
+                     point.schedule,
+                     point.latlng[0],
+                     point.latlng[1],
+                     point.altitude])
+        if point.path2next is not None:
+            pd.DataFrame(point.path2next).to_csv("app/static/data/mytrip_item/mytrip_"+pid_string+".csv",
+                                             index=False)
+
+    dftrip = pd.DataFrame(text,
+                          columns=["PID", "Day","placename","schedule","lat","lng","altitude"])
+    dftrip["PID"] = dftrip["PID"].astype('str')
+    dftrip.to_csv("app/static/data/mytrip.csv",index=False)
+
+
+
---
+++ static/js/googleMap.js
@@ -0,0 +1,168 @@
+
+function add_content_to_subsection(mytrip,pid){
+    content = get_PID_info(mytrip,pid)
+    document.getElementById("subsection_content").innerHTML = content
+}
+
+
+function get_PID_info(mytrip,i){
+    text = "<h2>" + mytrip.placename[i] + "</h2>";
+    text += "<h3>Day " + mytrip.Day[i].toString() +"</h3>";
+    text += "\n<h4>Altitude: " + getAltString(mytrip.altitude[i]) +"</h4>" + mytrip.schedule[i]
+    return(text)
+}
+
+function getAltString(meter){
+    meter = Math.round(meter);
+    feet = Math.round(meter * 3.28084);
+    return meter.toString() + "m " + "(" + feet.toString()+"feet)";
+}
+
+
+function display_map_on_page(){
+     var lukla = {lat: 27.695529, lng: 86.727778};
+     var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 9,
+          mapTypeId: google.maps.MapTypeId.HYBRID
+      });
+     map.setTilt(45);
+     return(map)
+}
+<!-- https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/ -->
+function initialize(mytrip, mytrip_item) {
+    // info window contents
+    // N of rows in my trip
+    Nitem = Object.keys(mytrip.placename).length
+    var map = display_map_on_page();
+    var bounds = new google.maps.LatLngBounds();
+
+
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    // Loop through our array of markers & place each one on the map
+
+    for( i = 0; i < Nitem; i++ ) {
+
+        var position = new google.maps.LatLng(mytrip.lat[i], mytrip.lng[i]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: i.toString(),
+            //label: mytrip.Day[i].toString()
+        });
+
+
+        // Allow each marker to have an info window
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                text = "<h2>" + mytrip.placename[i] + "</h2>";
+
+                infoWindow.setContent(text);
+                infoWindow.open(map, marker);
+                map.setCenter(marker.getPosition());
+                map.setZoom(10);
+
+                add_content_to_subsection(mytrip,i)
+
+                // trace
+                data = get_trace(mytrip_item[ mytrip.PID[i] ]);
+                Plotly.newPlot('plot_trace', data["trace"],data["layout"]);
+
+            }
+        })(marker, i));
+
+        if ( (Object.keys(mytrip_items)).length == 1){
+            // There is only a single item if mytrip contains only a single day info:
+            add_content_to_subsection(mytrip,i)
+        }else{
+           //  If mytrip contains all days..
+            document.getElementById("subsection_content").innerHTML = "<h2>Click markers in google map to see the planned route of the day</h2>"
+        }
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    llatlng = [];
+    for (point_key in mytrip_items){
+        point = mytrip_items[point_key];
+        for (item_key in point["lat"]){
+            llatlng.push({"lat":point["lat"][item_key],
+                          "lng":point["lng"][item_key]});
+        }
+
+        //https://developers.google.com/maps/documentation/javascript/examples/polyline-simple
+        var path = new google.maps.Polyline({
+                        path: llatlng,
+                        geodesic: true,
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 0.5,
+                        strokeWeight: 10});
+        path.setMap(map);
+    }
+
+
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    google.maps.event.trigger(map, 'resize');
+
+};
+
+
+function get_trace(mytrip){
+    var x, xlabel;
+    if (mytrip.distance != undefined){
+        x = Object.values(mytrip.distance);
+        xlabel = "Distance (km)";
+    }else{
+        x = Object.keys(mytrip.altitude);
+        xlabel = "Day"
+    }
+
+    var trace1 = {
+      x: x,
+      y: Object.values(mytrip.altitude),
+      type: 'scatter',
+      text: Object.values(mytrip.placename),
+      mode: 'lines+markers+text',
+      name: 'trace from Google Map'
+    };
+
+    data = {
+        "trace":[trace1],
+        "layout": {
+        autosize: true,
+        height: 200,
+        title:"Altitude changes",
+        margin: {l: 100,r: 100,b: 50,t: 30,pad: 0},
+        annotations: [{
+          xref: 'paper',
+          yref: 'paper',
+          x: 0,
+          xanchor: 'right',
+          y: 1,
+          yanchor: 'bottom',
+          text: 'Altitude (m)',
+          showarrow: false
+            }, {
+          xref: 'paper',
+          yref: 'paper',
+          x: 1,
+          xanchor: 'left',
+          y: 0,
+          yanchor: 'top',
+          text: xlabel,
+          showarrow: false
+        }]
+        //paper_bgcolor: '#7f7f7f',
+        //plot_bgcolor: '#c7c7c7'
+        }
+    };
+    return data;
+};
+
